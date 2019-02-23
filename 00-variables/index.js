bbe212efba94b64a09bf75ddc179c8da8fe61781
Buateng <elsharawy1630@gmail.com>
2019-02-23 12:25:46 -0600

    clase 1

---
+++ 00-variables/index.js
@@ -0,0 +1,44 @@
+//variables y constantes
+var nombre = "heman"; //variable global
+let apellido = "morales"; //variable local
+const edad = 25; //constante o dato parcialmente inmutalbe
+
+//tipos de datos
+let numeros = [1,3,3,4]; //Array o matriz
+//let objeto = {123};
+let texto = "hola"; //tipo string
+let cantidad = 22.2; // tipo numerico
+//Nan not a number
+//boolean = true o false
+
+
+//operadores aritmeticos
+// console.log(1+2);
+// console.log(1*2);
+// console.log(11/2);
+// console.log(11%2);
+
+//comparacion
+//< > <= >= != === ==
+
+// if(12%2 == 0){
+//     console.log("es par");
+// }else if(numeros[2]<4){
+//     console.log("es verdadero")
+// }
+
+
+// function comparar(edad){
+//     if((edad>2) ̣̣̣&&(edad<13)){
+//         console.log("es verdad");
+//     }
+// }
+
+
+for(let i=0;i<10;i+=3){
+    console.log(i);
+
+}
+
+
+
